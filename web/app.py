cda3abc22b59863e3665bddb0b89efaacf910567
Jordan <jordanmand@gmail.com>
2019-03-08 11:35:32 -0800

    Incorporate twitter search api request

--- web/app.py
+++ web/app.py
@@ -1,12 +1,20 @@
 from flask import Flask
-import requests
+import oauth2 as oauth
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    r = requests.get('https://twitter.com/')
-    return r.url
+    consumer = oauth.Consumer(
+        os.environ.get("TWITTER_API_KEY"),
+        os.environ.get("TWITTER_API_SECRET")
+    )
+    client = oauth.Client(consumer)
+
+    url = "https://api.twitter.com/1.1/search/tweets.json?q=covfefe"
+    resp, content = client.request(url, "GET")
+    return content
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
